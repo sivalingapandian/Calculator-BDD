cc3286a5704f2eb9f8446ebf5c50ffa100a670ac
Pandian Velayutham <Pandian.Velayutham@medhost.com>
2015-06-17 10:07:54 -0500

    Added BDD and it is still failing

---
+++ src/test/java/integration/feature/calculator/CalculatorOperationSteps.java
@@ -0,0 +1,9 @@
+package integration.feature.calculator;
+
+/**
+ * Test class for running steps
+ *
+ * Created by pandian on 6/17/15.
+ */
+public class CalculatorOperationSteps {
+}
---
+++ src/test/java/integration/feature/calculator/CalculatorTest.java
@@ -0,0 +1,13 @@
+package integration.feature.calculator;
+
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+/**
+ * Test class to trigger BDD framework
+ *
+ * Created by pandian on 6/17/15.
+ */
+@RunWith(Cucumber.class)
+public class CalculatorTest {
+}
