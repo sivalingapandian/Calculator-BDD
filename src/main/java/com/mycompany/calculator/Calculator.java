e0296b7de40eeaf9cd3e785b91c2e158a4f7b641
Pandian Velayutham <Pandian.Velayutham@medhost.com>
2015-06-17 09:34:49 -0500

    Implemented One method

--- src/main/java/com/mycompany/calculator/Calculator.java
+++ src/main/java/com/mycompany/calculator/Calculator.java
@@ -6,9 +6,24 @@ package com.mycompany.calculator;
  * Created by pandian on 6/17/15.
  */
 public class Calculator implements MathTool {
+
+    Double calculatorMathSummary = 0.0;
+
     @Override
     public Double performOperation(char operation, Double value) {
-        return null;
+        switch (operation) {
+            case '+': calculatorMathSummary = calculatorMathSummary + value;
+                break;
+            case '-': calculatorMathSummary = calculatorMathSummary - value;
+                break;
+            case '*': calculatorMathSummary = calculatorMathSummary * value;
+                break;
+            case '/': calculatorMathSummary = calculatorMathSummary / value;
+                break;
+            default:
+                break;
+        }
+        return calculatorMathSummary;
     }
 
     @Override
